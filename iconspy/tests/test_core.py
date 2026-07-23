f6ed2ad8dab8fbe31e31580ff393a13c15ea2b96
Fraser William Goldsworth <fraser.goldsworth@mpimet.mpg.de>
2026-07-23 18:38:29 +0200

    updated target station tests

--- iconspy/tests/test_core.py
+++ iconspy/tests/test_core.py
@@ -1,14 +1,69 @@
+import pytest
 from iconspy.core import *
 from .conftest import raw_grid, ispy_grid, boundary_target_station
 
 
 def test_TargetStation(ispy_grid):
+    # Do a boundary station test
     fram_strait_west = TargetStation("Fram Strait West", -14, 80)
+    ## Check type
     assert isinstance(fram_strait_west.to_model_station(ispy_grid), BoundaryModelStation)
     
+    ## Check attributes
+    assert str(fram_strait_west.name) == "Fram Strait West"
+    assert fram_strait_west.target_lon == -14
+    assert fram_strait_west.target_lat == 80
+    assert fram_strait_west.boundary is True
+
+    ## Check methods
+    fram_strait_west.to_model_station(ispy_grid)
+    fram_strait_west._to_boundary_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_west._to_any_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_west._to_wet_model_station(ispy_grid)
+
+
+    # Do a wet station test
     fram_strait_central = TargetStation("Fram Strait Central", 1, 80, boundary=False)
+    
+    ## Check type
     assert isinstance(fram_strait_central.to_model_station(ispy_grid), WetModelStation)
     
+    ## Check attributes
+    assert str(fram_strait_central.name) == "Fram Strait Central"
+    assert fram_strait_central.target_lon == 1
+    assert fram_strait_central.target_lat == 80
+    assert fram_strait_central.boundary is False
+    
+    ## Check methods
+    fram_strait_central.to_model_station(ispy_grid)
+    fram_strait_central._to_wet_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_central._to_boundary_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_central._to_any_model_station(ispy_grid)
+    
+    
+    # Do a non-guaranteed wet station test
+    fram_strait_central = TargetStation("Fram Strait Central", 1, 80, boundary=None)
+    
+    ## Check type
+    assert isinstance(fram_strait_central.to_model_station(ispy_grid), ModelStation)
+    
+    ## Check attributes
+    assert str(fram_strait_central.name) == "Fram Strait Central"
+    assert fram_strait_central.target_lon == 1
+    assert fram_strait_central.target_lat == 80
+    assert fram_strait_central.boundary is None
+
+    ## Check methods
+    fram_strait_central.to_model_station(ispy_grid)
+    fram_strait_central._to_any_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_central._to_boundary_model_station(ispy_grid)
+    with pytest.raises(ValueError):
+        fram_strait_central._to_wet_model_station(ispy_grid)
 
 def test_ModelStation(ispy_grid, boundary_target_station, wet_target_station):
     ds_IsD = ispy_grid
